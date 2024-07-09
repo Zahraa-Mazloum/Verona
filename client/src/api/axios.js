c8cb310b30a892dd34e2e0a86ba73d3862c846e1
Zahraa Mazloum <zahraamazloum2001@gmail.com>
2024-07-09 11:18:19 +0300

    date

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:'http://localhost:5001/api'
-    // baseURL:'https://verona-cfiw.onrender.com/api'
+    // baseURL:'http://localhost:5001/api'
+    baseURL:'https://verona-cfiw.onrender.com/api'
 });
