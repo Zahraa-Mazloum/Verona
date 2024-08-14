85733224ef407bd0b0243d1c17027351626fa896
zahraamazloum2001@gmail.com <zahraamazloum2001@gmail.com>
2024-08-14 09:10:15 +0300

    contract time

--- server/models/contractModel.js
+++ server/models/contractModel.js
@@ -1,86 +1,114 @@
-    import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-    const contractSchema = new mongoose.Schema({
-      investorInfo: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Investor', 
-        required: true },
-      amount: { 
-        type: Number, 
-        required: true },
-      currency: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Currency', 
-        required: true },
-      contractTime: {
-        type: String,
-          enum: ['month', 'year', 'day'], 
-        required: true },
-      contractTime_ar: { 
-        type: String, enum: ['شهر', 'سنة', 'يوم'],
-        required: true },
-      startDate: { 
-        type: Date, 
-        equired: true },
-      endDate: { type: Date },
-      contractPercentage: { type: Number },
-      profit: { type: Number },
-      payment: { type: Number },
-      investmentStatus: { type: Boolean, default: true },
-      withdraw: { type: Number},
-    }, { timestamps: true });
+const contractSchema = new mongoose.Schema({
+  investorInfo: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Investor', 
+    required: true 
+  },
+  amount: { 
+    type: Number, 
+    required: true 
+  },
+  currency: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Currency', 
+    required: true 
+  },
+  contractTime: {
+    type: String,
+    enum: ['month', 'Three months', 'Six months', 'year', 'day'], 
+    required: true 
+  },
+  contractTime_ar: { 
+    type: String, 
+    enum: ['شهر', 'سنة', 'يوم' ,'سته اشهر' , 'ثلاثة اشهر'],
+    required: true 
+  },
+  startDate: { 
+    type: Date, 
+    required: true 
+  },
+  endDate: { 
+    type: Date 
+  },
+  contractPercentage: { 
+    type: Number 
+  },
+  profit: { 
+    type: Number 
+  },
+  payment: { 
+    type: Number 
+  },
+  investmentStatus: { 
+    type: Boolean, 
+    default: true 
+  },
+  withdraw: { 
+    type: Number 
+  },
+}, { timestamps: true });
 
-    contractSchema.pre('save', function(next) {
-      if (this.isModified('startDate') || this.isNew) {
-        const startDate = this.startDate;
-        let endDate = new Date(startDate);
+contractSchema.pre('save', function(next) {
+  if (this.isModified('startDate') || this.isNew) {
+    const startDate = this.startDate;
+    let endDate = new Date(startDate);
 
-        if (this.contractTime === 'month' || this.contractTime === 'شهر') {
-          endDate.setMonth(startDate.getMonth() + 1);
-          this.contractPercentage = 2; 
-        } else if (this.contractTime === 'year' || this.contractTime === 'سنة') {
-          endDate.setFullYear(startDate.getFullYear() + 1);
-          this.contractPercentage = 36; 
-        } else if (this.contractTime === 'day' || this.contractTime === 'يوم') {
-          endDate.setDate(startDate.getDate() + 1);
-          this.contractPercentage = 2; 
-        }
-        this.endDate = endDate;
-      }
+    if (this.contractTime === 'month' || this.contractTime === 'شهر') {
+      endDate.setMonth(startDate.getMonth() + 1);
+      this.contractPercentage = 2; 
+    } else if (this.contractTime === 'year' || this.contractTime === 'سنة') {
+      endDate.setFullYear(startDate.getFullYear() + 1);
+      this.contractPercentage = 36; 
+    } else if (this.contractTime === 'Three months' || this.contractTime === 'ثلاثة اشهر') {
+      endDate.setMonth(startDate.getMonth() + 3);
+      this.contractPercentage = 6.9; 
+    } else if (this.contractTime === 'Six months' || this.contractTime === 'سته اشهر') {
+      endDate.setMonth(startDate.getMonth() + 6);
+      this.contractPercentage = 15.6; 
+    } else if (this.contractTime === 'day' || this.contractTime === 'يوم') {
+      endDate.setDate(startDate.getDate() + 1);
+      this.contractPercentage = 1; 
+    }
+    this.endDate = endDate;
+  }
 
-      if (this.isModified('amount') || this.isNew) {
-        this.profit = this.amount * (this.contractPercentage / 100);
-        this.payment = this.amount + this.profit;
-        this.withdraw = this.amount+ this.currentProfit;
-      }
+  if (this.isModified('amount') || this.isNew) {
+    this.profit = this.amount * (this.contractPercentage / 100);
+    this.payment = this.amount + this.profit;
+    this.withdraw = this.amount + this.currentProfit;
+  }
 
-      next();
-    });
+  next();
+});
 
-    contractSchema.virtual('isMatured').get(function() {
-      return new Date() >= this.endDate;
-    });
+contractSchema.virtual('isMatured').get(function() {
+  return new Date() >= this.endDate;
+});
 
-    contractSchema.virtual('currentProfit').get(function() {
-      const now = new Date();
-      if (now >= this.endDate) {
-        return this.profit;
-      }
-      
-      const duration = {
-        day: 24 * 60 * 60 * 1000,
-        month: 30 * 24 * 60 * 60 * 1000,
-        year: 365 * 24 * 60 * 60 * 1000
-      }[this.contractTime];
+contractSchema.virtual('currentProfit').get(function() {
+  const now = new Date();
+  if (now >= this.endDate) {
+    return this.profit;
+  }
 
-      const elapsedTime = now - this.startDate;
-      const elapsedPercentage = elapsedTime / duration;
-      return this.amount * (this.contractPercentage / 100) * elapsedPercentage;
-    });
+  const duration = {
+    day: 24 * 60 * 60 * 1000,
+    month: 30 * 24 * 60 * 60 * 1000,
+    'Three months': 3 * 30 * 24 * 60 * 60 * 1000, 
+    'Six months': 6 * 30 * 24 * 60 * 60 * 1000, 
+    year: 365 * 24 * 60 * 60 * 1000
+  }[this.contractTime];
 
-    contractSchema.set('toJSON', { virtuals: true });
-    contractSchema.set('toObject', { virtuals: true });
+  const elapsedTime = now - this.startDate;
+  const elapsedPercentage = elapsedTime / duration;
+  return this.amount * (this.contractPercentage / 100) * elapsedPercentage;
+});
 
-    const Contract = mongoose.model('Contract', contractSchema);
+contractSchema.set('toJSON', { virtuals: true });
+contractSchema.set('toObject', { virtuals: true });
 
-    export default Contract;
+const Contract = mongoose.model('Contract', contractSchema);
+
+export default Contract;
