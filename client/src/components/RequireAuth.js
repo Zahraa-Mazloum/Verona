48275cae057583160832f9b83c6d40570814c216
zahraamazloum2001@gmail.com <zahraamazloum2001@gmail.com>
2024-08-26 20:00:24 +0300

    login navigate

--- client/src/components/RequireAuth.js
+++ client/src/components/RequireAuth.js
@@ -1,18 +1,28 @@
+import { useRef } from 'react';
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Layout from "./layout";
+import { toast } from 'react-toastify'; 
 
 const RequireAuth = () => {
     const { auth } = useAuth();
     const location = useLocation();
+    const toastDisplayed = useRef(false); 
+
+    if (!auth?.token) {
+        if (!toastDisplayed.current) {
+            toast.error('You must log in first', {
+                toastId: 'auth-error', 
+            });
+            toastDisplayed.current = true;
+        }
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
 
     return (
-        auth?.token
-            ? <Layout>
-                <Outlet />
-            </Layout>
-            : <Navigate to="/login" state={{ from: location }} replace />
-
+        <Layout>
+            <Outlet />
+        </Layout>
     );
 };
 
